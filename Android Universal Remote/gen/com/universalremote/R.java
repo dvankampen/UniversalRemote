e4458d252dae24b3ae661a0655546be49f3243d7
dvankampen <dave.vankampen@gmail.com>
2012-11-16 19:26:06 -0500

    added new images for documentation

--- Android Universal Remote/gen/com/universalremote/R.java
+++ Android Universal Remote/gen/com/universalremote/R.java
@@ -157,13 +157,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button_cable=0x7f090003;
-        public static final int button_ps3=0x7f090004;
-        public static final int button_receiver=0x7f090002;
-        public static final int button_tv=0x7f090001;
-        public static final int group1=0x7f090006;
-        public static final int help=0x7f090007;
-        public static final int menu_settings=0x7f090005;
+        public static final int button_cable=0x7f090004;
+        public static final int button_ps3=0x7f090005;
+        public static final int button_receiver=0x7f090003;
+        public static final int button_tv=0x7f090002;
+        public static final int group1=0x7f090008;
+        public static final int help=0x7f090009;
+        public static final int linearLayout1=0x7f090001;
+        public static final int listView1=0x7f090006;
+        public static final int menu_settings=0x7f090007;
         public static final int selectDevice=0x7f090000;
     }
     public static final class layout {
