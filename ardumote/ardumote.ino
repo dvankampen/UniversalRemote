9aeda6ce8b9484128980803d418e337f5edf33c4
dvankampen <dave.vankampen@gmail.com>
2012-11-19 17:53:25 -0500

    more html updates

--- ardumote/ardumote.ino
+++ ardumote/ardumote.ino
@@ -5,9 +5,12 @@
  *
  */
 
-#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <pins_arduino.h>
+#include <SPI.h>
 #include <Ethernet.h>
-#include <EthernetUDP.h>
+#include <Flash.h>
+#include <SD.h>
+#include <TinyWebServer.h>
 
 #include <avr/pgmspace.h> // for progmem, for LED MIME images
 #define P(name)   static const prog_uchar name[] PROGMEM  // declare a static string
@@ -15,11 +18,167 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip[] =  { 192,168,1,9 };
 
+// pin 4 is the SPI select pin for the SDcard
+const int SD_CS = 4;
+
+// pin 10 is the SPI select pin for the Ethernet
+const int ETHER_CS = 10;
+
+/*********************************************/
+boolean has_filesystem = true;
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+SdFile file;
+
 const int MAX_PAGENAME_LEN = 8;  // max characters in page name 
 char buffer[MAX_PAGENAME_LEN+1]; // additional character for terminating null
 
 EthernetServer server(80);
 EthernetClient client;
+
+// The LED attached to PIN 22 on an Arduino board.
+const int LEDPIN = 22;
+
+// The initial state of the LED
+int ledState = LOW;
+
+void setLedEnabled(boolean state) {
+  ledState = state;
+  digitalWrite(LEDPIN, ledState);
+}
+
+boolean index_handler(TinyWebServer& web_server);
+
+void send_file_name(TinyWebServer& web_server, const char* filename) {
+  if (!filename) {
+    web_server.send_error_code(404);
+    web_server << F("Could not parse URL");
+  } else {
+    TinyWebServer::MimeType mime_type
+      = TinyWebServer::get_mime_type_from_filename(filename);
+    web_server.send_error_code(200);
+    web_server.send_content_type(mime_type);
+    web_server.end_headers();
+    if (file.open(&root, filename, O_READ)) {
+      Serial << F("Read file "); Serial.println(filename);
+      web_server.send_file(file);
+      file.close();
+    } else {
+      web_server << F("Could not find file: ") << filename << "\n";
+    }
+  }
+}
+
+TinyWebServer::PathHandler handlers[] = {
+  {"/upload/" "*", TinyWebServer::PUT, &TinyWebPutHandler::put_handler },
+  {"/tv/",TinyWebServer::GET, &tv_command_handler },
+  {"/receiver/",TinyWebServer::GET, &receiver_command_handler },
+  {"/cable/",TinyWebServer::GET, &cable_command_handler },
+  {"/", TinyWebServer::GET, &index_handler },
+  {NULL},
+};
+
+const char* headers[] = {
+  "Content-Length",
+  NULL
+};
+
+boolean file_handler(TinyWebServer& web_server) {
+  char* filename = TinyWebServer::get_file_from_path(web_server.get_path());
+  send_file_name(web_server, filename);
+  free(filename);
+  return true;
+}
+
+boolean learner_handler(TinyWebServer& web_server) {}
+
+boolean tv_command_handler(TinyWebServer& web_server) {
+  send_file_name(web_server, "TV.HTM");
+  if (client.available()) {
+    char ch = (char)client.read();
+    if (ch == '0') {
+      setLedEnabled(false);
+    } else if (ch == '1') {
+      setLedEnabled(true);
+    }
+  }
+}
+
+boolean cable_command_handler(TinyWebServer& web_server) {
+  send_file_name(web_server, "CABLE.HTM");
+  
+  Client& client = web_server.get_client();
+  if (client.available()) {
+    char ch = (char)client.read();
+    if (ch == '0') {
+      setLedEnabled(false);
+    } else if (ch == '1') {
+      setLedEnabled(true);
+    }
+  }
+}
+
+boolean receiver_command_handler(TinyWebServer& web_server) {
+  send_file_name(web_server, "RECEIVER.HTM");
+  if (client.available()) {
+    char ch = (char)client.read();
+    if (ch == '0') {
+      setLedEnabled(false);
+    } else if (ch == '1') {
+      setLedEnabled(true);
+    }
+  }
+}
+
+boolean index_handler(TinyWebServer& web_server) {
+  send_file_name(web_server, "INDEX.HTM");
+  return true;
+}
+
+void file_uploader_handler(TinyWebServer& web_server,
+                           TinyWebPutHandler::PutAction action,
+                           char* buffer, int size) {
+  static uint32_t start_time;
+
+  switch (action) {
+  case TinyWebPutHandler::START:
+    start_time = millis();
+    if (!file.isOpen()) {
+      // File is not opened, create it. First obtain the desired name
+      // from the request path.
+      char* fname = web_server.get_file_from_path(web_server.get_path());
+      if (fname) {
+        Serial << "Creating " << fname << "\n";
+        file.open(&root, fname, O_CREAT | O_WRITE | O_TRUNC);
+        free(fname);
+      }
+    }
+    break;
+
+  case TinyWebPutHandler::WRITE:
+    if (file.isOpen()) {
+      file.write(buffer, size);
+    }
+    break;
+
+  case TinyWebPutHandler::END:
+    file.sync();
+    Serial << "Wrote " << file.fileSize() << " bytes in "
+           << millis() - start_time << " millis\n";
+    file.close();
+  }
+}
+
+boolean has_ip_address = false;
+TinyWebServer web = TinyWebServer(handlers, headers);
+
+const char* ip_to_str(const uint8_t* ipAddr)
+{
+  static char buf[16];
+  sprintf(buf, "%d.%d.%d.%d\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
+  return buf;
+}
 
 EthernetUDP Udp;
 
@@ -33,61 +192,59 @@ char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 void setup()
 {
   Serial.begin(115200);
+  Serial << F("Free RAM: ") << FreeRam() << "\n";
+
+  pinMode(SS_PIN, OUTPUT);	// set the SS pin as an output
+                                // (necessary to keep the board as
+                                // master and not SPI slave)
+  digitalWrite(SS_PIN, HIGH);	// and ensure SS is high
+
+  // Ensure we are in a consistent state after power-up or a reset
+  // button These pins are standard for the Arduino w5100 Rev 3
+  // ethernet board They may need to be re-jigged for different boards
+  pinMode(ETHER_CS, OUTPUT);	// Set the CS pin as an output
+  digitalWrite(ETHER_CS, HIGH);	// Turn off the W5100 chip! (wait for
+                                // configuration)
+  pinMode(SD_CS, OUTPUT);	// Set the SDcard CS pin as an output
+  digitalWrite(SD_CS, HIGH);	// Turn off the SD card! (wait for
+                                // configuration)
+                                
   Ethernet.begin(mac, ip);
-  server.begin();
+  // initialize the SD card.
+  Serial << F("Setting up SD card...\n");
+  // pass over the speed and Chip select for the SD card
+  if (!card.init(SPI_FULL_SPEED, SD_CS)) {
+    Serial << F("card failed\n");
+    has_filesystem = false;
+  }
+  // initialize a FAT volume.
+  if (!volume.init(&card)) {
+    Serial << F("vol.init failed!\n");
+    has_filesystem = false;
+  }
+  if (!root.openRoot(&volume)) {
+    Serial << F("openRoot failed");
+    has_filesystem = false;
+  }
+
+  if (has_filesystem) {
+    // Assign our function to `upload_handler_fn'.
+    TinyWebPutHandler::put_handler_fn = file_uploader_handler;
+  }
+
+  // Start the web server.
+  Serial << F("Web server starting...\n");
+  web.begin();
+
+  Serial << F("Ready to accept HTTP requests.\n\n");
   Udp.begin(localPort);
   delay(1000);
-  Serial.println(F("Ready"));
 }
 
 void loop()
 {
 
-  client = server.available();
-  if (client) {
-    int type = 0;
-    while (client.connected()) {
-      if (client.available()) {
-        // GET, POST, or HEAD
-        memset(buffer,0, sizeof(buffer)); // clear the buffer
-        if(client.readBytesUntil('/', buffer,MAX_PAGENAME_LEN)){ 
-          if(strcmp(buffer, "GET ") == 0 )
-            type = 1;
-          else if(strcmp(buffer,"POST ") == 0)
-            type = 2;
-          // look for the page name
-          memset(buffer,0, sizeof(buffer)); // clear the buffer
-          if(client.readBytesUntil( '/', buffer,MAX_PAGENAME_LEN )) 
-          {
-            if (strcasecmp(buffer, "home") == 0) 
-              showHome();
-            else if (strcasecmp(buffer, "lcd") == 0) 
-              showLCD();
-            else if (strcasecmp(buffer, "tv") == 0) 
-              showTV();
-            else if (strcasecmp(buffer, "receiver") == 0) 
-              showReceiver();
-            else if (strcasecmp(buffer, "cable") == 0) 
-              showCable();
-            else if (strcasecmp(buffer, "status") == 0) 
-              showStatus();
-            else if(strcasecmp(buffer, "analog") == 0)
-              showAnalog();
-            else if(strcasecmp(buffer, "digital") == 0)
-              showDigital();
-            else if(strcmp(buffer, "change")== 0)
-              showChange(type == 2);
-            else
-              unknownPage(buffer);
-          }
-        }
-        break;
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    client.stop();
-  }
+  web.process();
   
   packetSize = Udp.parsePacket();
   if (packetSize) {
--- html/upload_files.py
+++ html/upload_files.py
@@ -1,8 +1,57 @@
-import os, subprocess
+############################
+#
+# IMPORTS
+#
+############################
+import sys, os, subprocess, getopt
+############################
+#
+# GLOBALS
+#
+############################
+html = 0
+javascript = 0
+css = 0
+############################
+#
+# SUBROUTINES
+#
+############################
+def upload(file):
+	if os.path.isdir(file) == True:
+		upload(file) #recursively browse directory tree downward
+	else:
+		if (not(file.endswith('.py'))): #upload everything but this script!
+			if (((file.endswith('.htm')) and (html == 1)) or ((file.endswith('.js')) and (javascript == 1)) or ((file.endswith('.css')) and (css == 1))):
+				print "uploading " + file
+				subprocess.call("curl -v -0 -T " + file + " http://192.168.1.9/upload/")
 
-files = filter(os.path.isfile, os.listdir('.')) 
+				
+############################
+#
+# MAIN PROGRAM
+#
+############################
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hjc",["html","javascript", "css"]) 
+except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+if (not(opts)):
+	print "including all file types"
+	html = 1
+	javascript = 1
+	css = 1
+for opt, arg in opts:
+	if opt in ("-h", "--html"):
+		html = 1
+	elif opt in ("-j", "--javascript"):
+		javascript = 1
+	elif opt in ("-c", "--css"):
+		css = 1
+
+files = os.listdir('.')
 
 for file in files:	
-	if (file.endswith('.HTM')):
-		print "uploading " + file
-		subprocess.call("curl -v -0 -T " + file + " http://192.168.1.9/upload/")
+	upload(file)
+	
